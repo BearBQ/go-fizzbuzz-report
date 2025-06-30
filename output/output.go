d44ab94e79303b0d89599a32d528a159413e110e
Dmitry <d.a.demkin@gmail.com>
2025-06-30 21:48:24 +0500

    исправление программы согласно ревью 2

--- output/output.go
+++ output/output.go
@@ -1,21 +1,30 @@
 package output
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
-func PrintFizzBuzz(incoming [][3]string) error { //функция печати
+var Stdout io.Writer = os.Stdout
+
+// PrintFizzBuzz - выводит на печать последовательность fizzbuzz
+func PrintFizzBuzz(incoming [][3]string) error {
+
 	if incoming == nil {
 		return fmt.Errorf("пустой вывод данных")
 	}
-	for n, _ := range incoming {
-		fmt.Printf("%s      %s      %s\n", incoming[n][0], incoming[n][1], incoming[n][2])
+	for _, row := range incoming {
+		fmt.Fprintf(Stdout, "%s      %s      %s\n", row[0], row[1], row[2])
 	}
 	return nil
 }
 
-func PrintFizzBuzzNumb(incoming [4]int) error { //функция печати
+// PrintFizzBuzzNumb - выводит на печать количество элементов по типам
+func PrintFizzBuzzNumb(incoming [4]int) error {
 	if incoming == [4]int{0, 0, 0, 0} {
 		return fmt.Errorf("на печать пришли нулевые значения")
 	}
-	fmt.Printf("Количество FizzBuzz - %v, Количество Fizz - %v, Количество Buzz - %v, Количество числовых значений - %v \n", incoming[0], incoming[1], incoming[2], incoming[3])
+	fmt.Fprintf(Stdout, "Количество FizzBuzz - %v, Количество Fizz - %v, Количество Buzz - %v, Количество числовых значений - %v \n", incoming[0], incoming[1], incoming[2], incoming[3])
 	return nil
 }
